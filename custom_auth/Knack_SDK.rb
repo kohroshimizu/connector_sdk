102bdeb828647568e246cc554898d9c41f9f8e06
Kohro Shimizu <39761424+kohroshimizu@users.noreply.github.com>
2018-07-13 13:12:32 +0800

    Update Knack_SDK.rb

--- custom_auth/Knack_SDK.rb
+++ custom_auth/Knack_SDK.rb
@@ -277,38 +277,47 @@
                       { name: "url" }]
                       }
                   elsif f["type"] == 'link'
-                    { name: f["key"], label: f["label"].labelize,
+                    {
+                      name: f["key"], label: f["label"].labelize,
                       type: :object, properties: [
-                      { name: "url" }]
-                      }
+                      { name: "url" }
+                      ]
+                    }
                   elsif f["type"] == 'email'
-                    { name: f["key"], label: f["label"].labelize,
+                    {
+                      name: f["key"], label: f["label"].labelize,
                       type: :object, properties: [
-                      { name: "email" }]
-                      }
-                  elsif f["type"] == 'timer'
-                    { name: f["key"], label: f["label"].labelize,
+                      { name: "email" }
+                      ]
+                    }
+                  elsif f["type"] == "timer"
+                    {
+                      name: f["key"], label: f["label"].labelize,
                       type: :object, properties: [
-                      { name: "times", type: :array, of: :object, properties: [
-                        { name: "from", type: :object, properties: [
-                          { name: "date", type: :date_time }]
-                          },
-                        { name: "to", type: :object, properties: [
-                          { name: "date", type: :date_time }]
-                          }]
-                        }]
-                      }
-                  elsif f["type"] == 'date_time'
-                    { name: f["key"], label: f["label"].labelize,
-                      type: :object, properties: [
-                      { name: "date", type: :date_time }]
-                      }
+                      {
+                        name: "times", type: :array, of: :object, properties: [
+                        {
+                          name: "from", type: :object, properties: [
+                          { name: "date", type: :date_time }
+                          ] },
+                        {
+                          name: "to", type: :object, properties:
+                          [
+                          { name: "date", type: :date_time }
+                          ] }] }] }
+                  elsif f["type"] == "date_time"
+                    {
+                      name: f["key"], label: f["label"].labelize,
+                      type: :object, properties:
+                      [
+                      { name: "date", type: :date_time }
+                      ] }
                   else
                     { name: f["key"], label: f["label"].labelize,
                       type: f["type"] }
                   end
                 end
-              )
+            )
       }
     }
   },
